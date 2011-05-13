5d8f64125b6b14d0c2bc6d547fe44eb22141a2e1
George Gastaldi <gegastaldi@gmail.com>
2011-05-13 01:23:34 -0300

    Changing hierarchy of SeamReportException

--- api/src/main/java/org/jboss/seam/reports/SeamReportException.java
+++ api/src/main/java/org/jboss/seam/reports/SeamReportException.java
@@ -1,8 +1,7 @@
 package org.jboss.seam.reports;
 
-import java.io.IOException;
 
-public class SeamReportException extends IOException {
+public class SeamReportException extends RuntimeException {
 
     private static final long serialVersionUID = 1L;
 

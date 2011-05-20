d0272b056f1054f17bc6a219e662a9ce6856ca10
George Gastaldi <gegastaldi@gmail.com>
2011-05-20 10:59:20 -0300

    Removed unecessary annotations

--- api/src/main/java/org/jboss/seam/reports/output/PDF.java
+++ api/src/main/java/org/jboss/seam/reports/output/PDF.java
@@ -1,5 +1,11 @@
 package org.jboss.seam.reports.output;
 
+import static java.lang.annotation.ElementType.FIELD;
+import static java.lang.annotation.ElementType.METHOD;
+import static java.lang.annotation.ElementType.PARAMETER;
+import static java.lang.annotation.ElementType.TYPE;
+import static java.lang.annotation.RetentionPolicy.RUNTIME;
+
 import java.lang.annotation.Documented;
 import java.lang.annotation.Retention;
 import java.lang.annotation.Target;
@@ -7,13 +13,6 @@ import java.lang.annotation.Target;
 import javax.inject.Qualifier;
 
 import org.jboss.seam.reports.ReportRenderer;
-import org.jboss.seam.reports.spi.annotations.OutputBinding;
-
-import static java.lang.annotation.ElementType.FIELD;
-import static java.lang.annotation.ElementType.METHOD;
-import static java.lang.annotation.ElementType.PARAMETER;
-import static java.lang.annotation.ElementType.TYPE;
-import static java.lang.annotation.RetentionPolicy.RUNTIME;
 
 /**
  * Used on {@link ReportRenderer} 
@@ -24,7 +23,6 @@ import static java.lang.annotation.RetentionPolicy.RUNTIME;
 @Target({ TYPE, METHOD, PARAMETER, FIELD })
 @Retention(RUNTIME)
 @Documented
-@OutputBinding
 public @interface PDF {
 
 }
--- api/src/main/java/org/jboss/seam/reports/output/XLS.java
+++ api/src/main/java/org/jboss/seam/reports/output/XLS.java
@@ -1,24 +1,21 @@
 package org.jboss.seam.reports.output;
 
-import java.lang.annotation.Documented;
-import java.lang.annotation.Retention;
-import java.lang.annotation.Target;
-
-import javax.inject.Qualifier;
-
-import org.jboss.seam.reports.spi.annotations.OutputBinding;
-
 import static java.lang.annotation.ElementType.FIELD;
 import static java.lang.annotation.ElementType.METHOD;
 import static java.lang.annotation.ElementType.PARAMETER;
 import static java.lang.annotation.ElementType.TYPE;
 import static java.lang.annotation.RetentionPolicy.RUNTIME;
 
+import java.lang.annotation.Documented;
+import java.lang.annotation.Retention;
+import java.lang.annotation.Target;
+
+import javax.inject.Qualifier;
+
 @Qualifier
 @Target({ TYPE, METHOD, PARAMETER, FIELD })
 @Retention(RUNTIME)
 @Documented
-@OutputBinding
 public @interface XLS {
 
 }

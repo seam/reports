df5c85ca7e8bab51441b6b4af785627991afe5a2
George Gastaldi <gegastaldi@gmail.com>
2011-05-21 21:40:15 -0700

    Improved javadocs

--- api/src/main/java/org/jboss/seam/reports/Report.java
+++ api/src/main/java/org/jboss/seam/reports/Report.java
@@ -3,14 +3,14 @@ package org.jboss.seam.reports;
 import java.util.Map;
 
 /**
- * A compiled report object. May be viewed as a template object.
+ * A compiled report object. May be interpreted as a template object.
  * 
- * This object are normally created using a {@link ReportLoader}.
+ * This object is normally created using a {@link ReportLoader}.
  * 
  * Produces {@link ReportInstance} objects by filling them with a {@link ReportDataSource} object and some optional
  * parameters
  * 
- * @author george
+ * @author George Gastaldi
  * 
  */
 public interface Report {

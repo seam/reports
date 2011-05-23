232f967876ba161155ba86d397247a44ca14808d
George Gastaldi <gegastaldi@gmail.com>
2011-05-22 23:22:38 -0300

    Added javadocs

--- api/src/main/java/org/jboss/seam/reports/ReportDataSource.java
+++ api/src/main/java/org/jboss/seam/reports/ReportDataSource.java
@@ -1,5 +1,11 @@
 package org.jboss.seam.reports;
 
+/**
+ * Markup interface for report data sources. The underlying implementation may expose impl objects.
+ * 
+ * @author George Gastaldi
+ * 
+ */
 public interface ReportDataSource {
 
 }

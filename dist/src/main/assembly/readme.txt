bde9a42697940a2252ba720e1ae29e571aee887d
George Gastaldi <gegastaldi@gmail.com>
2011-06-08 23:14:53 -0300

    Preparation for release

--- dist/src/main/assembly/readme.txt
+++ dist/src/main/assembly/readme.txt
@@ -1,6 +1,6 @@
 
-                              Seam Reports Module
-                               3.0.0 SNAPSHOT
+Seam Reports Module
+${project.version}
 
  What is it?
  ===========
@@ -12,33 +12,46 @@
  Contents of distribution
  ========================
 
- docs/
+
+ artifacts/
+ 
+    Provided libraries
+
+ lib/
+
+    Dependencies
+
+ doc/
 
     API Docs and reference guide.
   
- examples/
-
-    Seam Faces examples
+ source/
+ 
+    Source code for this module
   
- ...
   
  Licensing
  =========
 
  This distribution, as a whole, is licensed under the terms of the Apache
- License, Version 2.0 (see apl.txt).
+ License, Version 2.0 (see license.txt).
 
  Seam Reports URLs
  ===============
 
  Seam 3 Reports Module:  http://www.sfwk.org/Seam3/ReportsModule
- Seam 3 Home Page:     http://www.sfwk.org/Seam3
- Downloads:            http://www.sfwk.org/Seam3/DistributionDownloads
- Forums:               http://www.sfwk.org/Community/Seam3Users
- Source Code:          git://github.com/seam/faces.git
- Issue Tracking:       http://jira.jboss.org/jira/browse/SEAMFACES
+ Seam 3 Home Page:       http://www.sfwk.org/Seam3
+ Downloads:              http://www.sfwk.org/Seam3/DistributionDownloads
+ Forums:                 http://www.sfwk.org/Community/Seam3Users
+ Source Code:            http://github.com/seam/reports
+ Issue Tracking:         http://jira.jboss.org/jira/browse/SEAMREPORTS
 
  Release Notes
  =============
-
-
+* [SEAMREPORTS-2] - Add support for JasperReports as a report engine
+* [SEAMREPORTS-3] - Create domain objects for compilation, population and rendering of reports 
+* [SEAMREPORTS-5] - Add support for Pentaho Reporting Engine
+* [SEAMREPORTS-13] - Create a seam forge plugin for  Seam Reports
+* [SEAMREPORTS-6] - Register jobs on Hudson 
+* [SEAMREPORTS-11] - Add jPdfUnit Maven Repository
+* [SEAMREPORTS-12] - Split impl into different modules
---
+++ impl/jasper/src/test/java/org/jboss/seam/reports/jasperreports/qualifiers/test/JasperReportsQualifierTest.java
@@ -0,0 +1,96 @@
+/**
+ * JBoss, Home of Professional Open Source
+ * Copyright 2011, Red Hat, Inc. and/or its affiliates, and individual
+ * contributors by the @authors tag. See the copyright.txt in the
+ * distribution for a full listing of individual contributors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.jboss.seam.reports.jasperreports.qualifiers.test;
+
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.util.Map;
+
+import javax.inject.Inject;
+
+import org.jboss.arquillian.api.Deployment;
+import org.jboss.arquillian.junit.Arquillian;
+import org.jboss.seam.reports.Report;
+import org.jboss.seam.reports.ReportDataSource;
+import org.jboss.seam.reports.ReportDefinition;
+import org.jboss.seam.reports.ReportRenderer;
+import org.jboss.seam.reports.jasper.Jasper;
+import org.jboss.seam.reports.jasper.JasperSeamReportLoader;
+import org.jboss.seam.reports.output.PDF;
+import org.jboss.seam.solder.resourceLoader.Resource;
+import org.jboss.shrinkwrap.api.ArchivePaths;
+import org.jboss.shrinkwrap.api.ShrinkWrap;
+import org.jboss.shrinkwrap.api.asset.EmptyAsset;
+import org.jboss.shrinkwrap.api.spec.JavaArchive;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import de.oio.jpdfunit.DocumentTester;
+
+@RunWith(Arquillian.class)
+public class JasperReportsQualifierTest {
+
+    @Inject
+    @SalesReport
+    ReportDefinition<ReportDataSource,Report> salesReport;
+
+    @Inject
+    @SalesReport
+    Map<String, Object> reportParams;
+
+    @Inject
+    @SalesReport
+    @Resource("XlsDataSource.data.xls")
+    ReportDataSource dataSource;
+
+    @Inject
+    @Jasper
+    @PDF
+    ReportRenderer<Report> pdfRenderer;
+
+    @Deployment
+    public static JavaArchive createArchive() {
+        return ShrinkWrap.create(JavaArchive.class)
+                .addPackages(true, "org.jboss.seam.solder")
+                .addPackages(true, "org.jboss.seam.reports")
+                .addPackages(true, "org.jboss.seam.reports.annotations")
+                .addPackages(true, "org.jboss.seam.reports.jasper")
+                .addClass(JasperSeamReportLoader.class)
+                .addAsManifestResource(EmptyAsset.INSTANCE, ArchivePaths.create("beans.xml"));
+    }
+
+    /**
+     * Lifecycle is Compile, populate, render
+     * 
+     * @throws Exception
+     */
+    @Test
+    public void testReportLifecycle() throws Exception {
+        Report reportInstance = salesReport.fill(dataSource, reportParams);
+
+        ByteArrayOutputStream os = new ByteArrayOutputStream(); // OutputStream
+       
+        // Render output as the desired content
+        pdfRenderer.render(reportInstance, os);
+        DocumentTester tester = new DocumentTester(new ByteArrayInputStream(os.toByteArray()));
+        try {
+            tester.assertPageCountEquals(2);
+        } finally {
+            tester.close();
+        }
+    }
+}
